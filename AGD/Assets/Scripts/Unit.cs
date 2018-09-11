06adc1fa04e372e2d63a291d0e8e301c7bdf8ed2
Stef <stef.beezhold@hva.nl>
2018-09-11 13:33:07 +0200

    Unit class

---
+++ AGD/Assets/Scripts/Unit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+	private int health;
+	private int steps;
+	private int damage;
+	private Type myClass;
+	private bool activated = false;
+	private bool selected = false;
+
+	public Unit(Type myClass, int health, int steps, int damage){
+		this.myClass = myClass;
+		this.health = health;
+		this.steps = steps;
+		this.damage = damage;
+	}
+	void DoDamage(int damage){
+		this.health -= damage;
+	}
+	void SetActivated(bool activated){
+		this.activated = activated;
+	}
+	bool GetActivated(){
+		return this.activated;
+	}
+	void SetSelected(bool selected){
+		this.selected = selected;
+	}
+	bool GetSelected(){
+		return this.selected;
+	}
+}
+public enum Type {Archer, Healer, Tank, Swordsman};

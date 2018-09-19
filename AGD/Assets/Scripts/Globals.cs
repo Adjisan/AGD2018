3dc8d814d9e33409774314909da7433afefe0496
Unknown <yosri.jerbi@hotmail.com>
2018-09-19 12:21:26 +0200

    Pre 3D Dump

--- AGD/Assets/Scripts/Globals.cs
+++ AGD/Assets/Scripts/Globals.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class Globals : MonoBehaviour {
 
-    public static float speed = 0.1f;
+    public static float speed = 0.05f;
     public static int lives = 3;
     public static int score = 0;
 
-    public static int picked = 0;
-    public static int start = 0;
-    public static int goal = 0;
 }

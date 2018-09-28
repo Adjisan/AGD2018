d611bf09518860bbc97b05b4df75f0a923d2c8ac
Naomi Vogelpoel <Naomi.Vogelpoel@hva.nl>
2018-09-28 10:38:19 +0200

    NextLevelRequirement
    Collision, not the actual level change yet

---
+++ AGD/Assets/NextLevelScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelScript : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "PlayerDrag")
+        {
+        Debug.Log("LevelWOn");
+        }
+    }
+}

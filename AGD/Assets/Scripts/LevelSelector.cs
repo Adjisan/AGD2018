8a3cde19dd6699be4e1d7c8e1b65d00e2fd90e13
Naomi Vogelpoel <Naomi.Vogelpoel@hva.nl>
2018-09-28 09:59:25 +0200

    Disable buttons
    all buttons but level 1

--- AGD/Assets/Scripts/LevelSelector.cs
+++ AGD/Assets/Scripts/LevelSelector.cs
@@ -8,12 +8,23 @@ public class LevelSelector : MonoBehaviour {
     public Button[] levelButtons;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 1 > levelReached)
+            {
+                levelButtons[i].interactable = false;
+            }
+        }	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
 }
+//this was created using the following tutorial: https://www.youtube.com/watch?v=AQpDtrNJAEU

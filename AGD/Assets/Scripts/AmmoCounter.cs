9b90eb0f44751d5819aae2419fec8da1bd4ecb75
Unknown <rizz_santosz@live.nl>
2018-10-04 11:48:27 +0200

    reworked the ammo counter
    that is supposed to work with the canvas

--- AGD/Assets/Scripts/AmmoCounter.cs
+++ AGD/Assets/Scripts/AmmoCounter.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class NewBehaviourScript : MonoBehaviour {
+
+public class AmmoCounter : MonoBehaviour {
 
     public int ammo = 30;
     public Text uitext;
@@ -14,6 +15,9 @@ public class NewBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(uitext != null)
+        {
+            uitext.text = ammo.ToString();
+        }
 	}
 }

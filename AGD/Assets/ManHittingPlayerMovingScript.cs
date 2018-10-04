fae3b620ba3a5933c5875bcd1abca87d35baedb1
Naomi Vogelpoel <Naomi.Vogelpoel@hva.nl>
2018-10-04 15:10:27 +0200

    Spawner works and AI works
    Warning: Do not activate all the cubes!!

---
+++ AGD/Assets/ManHittingPlayerMovingScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManHittingPlayerMovingScript : MonoBehaviour {
+    
+    Transform player;
+    UnityEngine.AI.NavMeshAgent nav;
+
+	// Use this for initialization
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("playerObj").transform;
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(player.position);
+	}
+}
---
+++ AGD/Assets/SpawnEnemy.cs
@@ -0,0 +1,32 @@
+﻿// This was made using the following tutorial https://www.youtube.com/watch?v=9KOHclqSmR4
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+
+    private float nextSpawnTime;
+
+    [SerializeField]
+    private GameObject enemyPrefab;
+    [SerializeField]
+    private float Delay;
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        if (ShouldSpawn())
+        {
+            Spawn();
+        }
+	}
+    private void Spawn()
+    {
+        nextSpawnTime = Time.time + Delay;
+        Instantiate(enemyPrefab, transform.position, transform.rotation);
+    }
+    private bool ShouldSpawn()
+    {
+        return Time.time > nextSpawnTime;
+    }
+}

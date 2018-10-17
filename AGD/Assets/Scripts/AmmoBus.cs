94c982508324788f53687d424ce640d3cd61bfce
Stef <stef.beezhold@hva.nl>
2018-10-17 11:03:54 +0200

    AmmoBus timing corrected

--- AGD/Assets/Scripts/AmmoBus.cs
+++ AGD/Assets/Scripts/AmmoBus.cs
@@ -7,12 +7,17 @@ public class AmmoBus : MonoBehaviour
     public bool open = true;
     public int force = 10;
     public int forceRight = 10;
+    public int forceDiagonal = 10;
+    public int forceThirdPhase = 20;
     public float targetTime = 5.0f;
-    public Rigidbody rb;
+    public float firstTimer = 5.0f, secondTimer = 5.0f, thirdTimer = 5.0f;
+    public int phase = 0;
 
+    public Rigidbody rb;
     // Use this for initialization
     void Start()
     {
+        targetTime = firstTimer;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -32,22 +37,44 @@ public class AmmoBus : MonoBehaviour
 
     void Update()
     {
-
         targetTime -= Time.deltaTime;
 
         if (targetTime <= 0.0f)
         {
-            // do stuff
-			if(forceRight == 0)
-			{
-				forceRight = force;
-			} else
-			{
-				forceRight = 0;
-			}
-            	
-			targetTime = 5.0f;
+            phase += 1;
+            switch (phase)
+            {
+                case 0:
+                    Debug.Log("first phase");
+                    targetTime = firstTimer;
+                    break;
+                case 1:
+                    Debug.Log("second phase");
+                    targetTime = secondTimer;
+                    break;
+                case 2:
+                    Debug.Log("third phase");
+                    targetTime = thirdTimer;
+                    break;
+            }
+            
+            if (phase == 0 || phase == 2)
+            {
+                forceRight = 0;
+            }
+            if (phase == 1)
+            {
+                forceRight = forceDiagonal;
+            }
+            if(phase == 2)
+            {
+                force = forceThirdPhase;
+            }
+            if (phase > 2)
+            {
+                Debug.Log("Destroy the AmmoBus");
+                phase = 0;
+            }
         }
-
     }
 }

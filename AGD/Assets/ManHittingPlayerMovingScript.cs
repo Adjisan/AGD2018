336048843109067ad087461c702731944e250164
Naomi Vogelpoel <Naomi.Vogelpoel@hva.nl>
2018-10-29 15:29:29 +0100

    Stars appear when you hit an enemy

--- AGD/Assets/ManHittingPlayerMovingScript.cs
+++ AGD/Assets/ManHittingPlayerMovingScript.cs
@@ -7,20 +7,16 @@ public class ManHittingPlayerMovingScript : MonoBehaviour {
     Transform player;
     UnityEngine.AI.NavMeshAgent nav;
     GameObject mailBox;
-    public Animator animator;
-    public TrailRenderer renderer;
+    public GameObject stars;
+    public GameObject angrySign;
 
     // Use this for initialization
     void Awake () {
         player = GameObject.FindGameObjectWithTag("playerObj").transform;
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
         mailBox = GameObject.FindGameObjectWithTag("MailBox");
-
-        animator = this.GetComponentInChildren<Animator>();
-        animator.SetBool("IsConfused", false);
-
-        renderer = this.GetComponentInChildren<TrailRenderer>();
-        renderer.enabled = false;
+        stars.SetActive(false);
+        angrySign.SetActive(true);
     }
 	
 	// Update is called once per frame
@@ -36,10 +32,10 @@ public class ManHittingPlayerMovingScript : MonoBehaviour {
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "projectile")
-        {
-            animator.SetBool("IsConfused", true);
-            renderer.enabled = true;
+        { 
             Debug.Log("destroy");
+            stars.SetActive(true);
+            angrySign.SetActive(false);
             gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
             Destroy(other.gameObject);
         }

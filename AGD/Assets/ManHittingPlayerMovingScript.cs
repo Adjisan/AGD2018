c6f15157776403509ee58f87949dbc39b0198694
Naomi Vogelpoel <Naomi.Vogelpoel@hva.nl>
2018-10-29 09:10:10 +0100

    Star before the rework

--- AGD/Assets/ManHittingPlayerMovingScript.cs
+++ AGD/Assets/ManHittingPlayerMovingScript.cs
@@ -8,14 +8,19 @@ public class ManHittingPlayerMovingScript : MonoBehaviour {
     UnityEngine.AI.NavMeshAgent nav;
     GameObject mailBox;
     public Animator animator;
+    public TrailRenderer renderer;
 
     // Use this for initialization
     void Awake () {
         player = GameObject.FindGameObjectWithTag("playerObj").transform;
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
         mailBox = GameObject.FindGameObjectWithTag("MailBox");
+
         animator = this.GetComponentInChildren<Animator>();
         animator.SetBool("IsConfused", false);
+
+        renderer = this.GetComponentInChildren<TrailRenderer>();
+        renderer.enabled = false;
     }
 	
 	// Update is called once per frame
@@ -33,13 +38,13 @@ public class ManHittingPlayerMovingScript : MonoBehaviour {
         if (other.gameObject.tag == "projectile")
         {
             animator.SetBool("IsConfused", true);
+            renderer.enabled = true;
             Debug.Log("destroy");
             gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
             Destroy(other.gameObject);
         }
         else
         {
-
             Debug.Log(other.gameObject);
         }
     }

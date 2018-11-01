31d043943f79099f9bfca7937fe2b7801c3353a4
Unknown <yosri.jerbi@hotmail.com>
2018-11-01 12:51:56 +0100

    Created prefabs

---
+++ AGD/Assets/Scripts/CancelKinematic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CancelKinematic : MonoBehaviour {
+    public string uponCollisionWith = "projectile";
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == uponCollisionWith) {
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().AddRelativeForce(collision.relativeVelocity,ForceMode.VelocityChange);
+        }
+    }
+}

8389699f50a3d082c86d4df4dbff040508d7f478
Stef <stef.beezhold@hva.nl>
2018-11-02 13:37:15 +0100

    AmmoBus waypoint movement and waypoint gizmos prefab

--- AGD/Assets/Scripts/AmmoBus.cs
+++ AGD/Assets/Scripts/AmmoBus.cs
@@ -5,20 +5,18 @@ using UnityEngine;
 public class AmmoBus : MonoBehaviour
 {
     public bool open = true;
-    public int force = 10;
-    public int forceRight = 10;
-    public int forceDiagonal = 10;
-    public int forceThirdPhase = 20;
-    public float targetTime = 5.0f;
-    public float firstTimer = 5.0f, secondTimer = 5.0f, thirdTimer = 5.0f;
-    public int phase = 0;
-
     public Rigidbody rb;
-    // Use this for initialization
+    public GameObject[] waypoints;
+    public float[] waypointSpeed;
+    int currentWaypointSpeed = 0;
+    int currentWaypoint = 0;
+    public float speed;
+    float waypointRadius = 1;
+
     void Start()
     {
-        targetTime = firstTimer;
         rb = GetComponent<Rigidbody>();
+        speed = waypointSpeed[currentWaypointSpeed];
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -30,51 +28,23 @@ public class AmmoBus : MonoBehaviour
             Destroy(collision.gameObject);
         }
     }
-    void FixedUpdate()
-    {
-        rb.MovePosition(transform.position + (forceRight * transform.right + force * transform.forward) * Time.deltaTime);
-    }
 
     void Update()
     {
-        targetTime -= Time.deltaTime;
-
-        if (targetTime <= 0.0f)
+        if(Vector3.Distance(waypoints[currentWaypoint].transform.position, transform.position) < waypointRadius)
         {
-            phase += 1;
-            switch (phase)
-            {
-                case 0:
-                    Debug.Log("first phase");
-                    targetTime = firstTimer;
-                    break;
-                case 1:
-                    Debug.Log("second phase");
-                    targetTime = secondTimer;
-                    break;
-                case 2:
-                    Debug.Log("third phase");
-                    targetTime = thirdTimer;
-                    break;
-            }
+            currentWaypoint++;
+            currentWaypointSpeed++;
             
-            if (phase == 0 || phase == 2)
+            if (currentWaypoint >= waypoints.Length)
             {
-                forceRight = 0;
+                currentWaypoint = 0;
+                currentWaypointSpeed = 0;
+                Destroy(this.gameObject);
+                Debug.Log("Reached last waypoint");
             }
-            if (phase == 1)
-            {
-                forceRight = forceDiagonal;
-            }
-            if(phase == 2)
-            {
-                force = forceThirdPhase;
-            }
-            if (phase > 2)
-            {
-                Debug.Log("Destroy the AmmoBus");
-                phase = 0;
-            }
+            speed = waypointSpeed[currentWaypointSpeed];
         }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].transform.position, Time.deltaTime * speed);
     }
 }

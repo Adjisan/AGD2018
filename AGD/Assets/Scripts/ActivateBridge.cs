c5b1812794fde3d24aa819c3557b2fe42e338c86
Unknown <yosri.jerbi@hotmail.com>
2018-11-02 16:20:19 +0100

    Created bridge prefabs

---
+++ AGD/Assets/Scripts/ActivateBridge.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateBridge : MonoBehaviour {
+    public string uponCollisionWith = "projectile";
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == uponCollisionWith) {
+            GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Animator>().enabled = true;
+        }
+    }
+}
---
+++ AGD/Assets/Scripts/AmmoHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoHandler : MonoBehaviour {
+    public int ammo;
+    public bool collidesWithTrigger = false;
+    public string collidesWithTag = "Player";
+
+    private bool triggered = false;
+    private GameManagerScript gmScript;
+
+    // Use this for initialization
+    void Start () {
+        gmScript = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider other) {
+        if (!collidesWithTrigger)
+            return;
+        if (other.transform.tag == collidesWithTag && !triggered) {
+            gmScript.DepleteAmmo(ammo);
+            gmScript.ammoCountText();
+            triggered = true;
+        }
+    }
+    private void OnCollisionEnter(Collision other) {
+        if (collidesWithTrigger)
+            return;
+        if (other.transform.tag == collidesWithTag && !triggered) {
+            gmScript.DepleteAmmo(ammo);
+            gmScript.ammoCountText();
+            triggered = true;
+        }
+    }
+}

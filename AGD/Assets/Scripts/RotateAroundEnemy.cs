c3a2ac414a3ed0ef17a5b234a82781cf991dc082
Naomi Vogelpoel <Naomi.Vogelpoel@hva.nl>
2018-12-20 10:22:43 +0100

    Deze versie is gebruikt voor mijn assessment

--- AGD/Assets/Scripts/RotateAroundEnemy.cs
+++ AGD/Assets/Scripts/RotateAroundEnemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RotateAroundEnemy : MonoBehaviour {
     public float enemyRotation;
+
+
 	// Update is called once per frame
 	void Update () {
         enemyRotation = GameObject.FindGameObjectWithTag("Enemy").transform.rotation.y;

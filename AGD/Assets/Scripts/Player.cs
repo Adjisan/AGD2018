c2a5ab315ea64b6db676d21a20424c63721e7921
Unknown <yosri.jerbi@hotmail.com>
2019-01-13 12:01:56 +0100

    Added countdownFeedback

--- AGD/Assets/Scripts/Player.cs
+++ AGD/Assets/Scripts/Player.cs
@@ -1,13 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour {
     public GameObject projectile;
+    public GameObject textPrefab;
+    private GameObject countDownClone;
+    private GameManagerScript gmScript;
+    public int maxCountDown = 5;
+
+    private void Start() {
+        countDownClone = Instantiate(textPrefab,transform);
+        gmScript = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+    }
     private void OnMouseDown() {
         if (GameObject.Find("GameManager").GetComponent<GameManagerScript>().ammo <= 0) return;
         Vector3 spawnPosition = new Vector3(transform.position.x, transform.position.y+1, transform.position.z);
         GameObject obj = Instantiate(projectile, spawnPosition, transform.rotation);
         obj.transform.parent = gameObject.transform;
+    }
+
+    private void Update() {
+        if (gmScript.ammo > maxCountDown) {
+            countDownClone.SetActive(false);
+        } else {
+            countDownClone.SetActive(true);
+            if (gmScript.ammo <=  0) {
+                countDownClone.GetComponent<TextMeshPro>().SetText("Empty!");
+                countDownClone.transform.localScale = textPrefab.transform.localScale / 2;
+            } else {
+                countDownClone.GetComponent<TextMeshPro>().SetText("" + gmScript.ammo);
+                countDownClone.transform.localScale = textPrefab.transform.localScale;
+            }
+            
+        }
     }
 }
